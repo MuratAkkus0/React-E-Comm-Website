daa4d3181d293be6339e169d0febb086715c256a
Murat Akkus <akkusmurat123@gmail.com>
2024-11-28 06:37:06 +0100

    .

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,12 @@
 import ProductList from "../components/productList";
 import "../assets/css/views/Home.css";
+import { useSelector } from "react-redux";
 
 function Home() {
+  const { isDarkTheme } = useSelector((store) => store.app);
   return (
     <>
-      <section className="page__container">
+      <section className={`page__container ${isDarkTheme ? "dark-theme" : ""}`}>
         <ProductList />
       </section>
     </>
