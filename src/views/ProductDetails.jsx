293f6976efa436a4be72d2b07de95ebde6f4926d
Murat Akkus <akkusmurat123@gmail.com>
2024-12-02 23:29:05 +0100

    go-back btn added

--- src/views/ProductDetails.jsx
+++ src/views/ProductDetails.jsx
@@ -1,14 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import AddToBasketBtn from "../components/AddToBasketBtn";
 import { useEffect } from "react";
 import { setSelectedProduct } from "../redux/slices/productSlice";
+import "../assets/css/views/ProductDetails.css";
+import { setIsBasketActive } from "../redux/slices/basketSlice";
+import { MdArrowBack } from "react-icons/md";
 
 function ProductDetails() {
   const { id } = useParams();
   const { products, selectedProduct } = useSelector((store) => store.products);
   const { title, description, image, price } = selectedProduct;
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -27,19 +30,23 @@ function ProductDetails() {
 
   return (
     <>
-      <section className={`page__container--flex-x-centered `}>
-        <div className="product__img--container">
-          <img
-            className="product__img--img"
-            src={image}
-            alt="product image"
-            width={300}
-          />
+      <section
+        onClick={() => dispatch(setIsBasketActive(false))}
+        className={`product__details--container page__container--flex-x-centered `}
+      >
+        <div
+          className="go-back flex-row-centered"
+          onClick={() => navigate("/")}
+        >
+          <MdArrowBack />
+        </div>
+        <div className="product__img--container flex-row-centered">
+          <img className="product__img--img" src={image} alt="product image" />
         </div>
         <div className="product__details">
           <div className="product__details--title">{title}</div>
           <div className="product__details--description">{description}</div>
-          <div className="product__details--price">{price}</div>
+          <div className="product__details--price">{price}€</div>
           <AddToBasketBtn product={selectedProduct} />
         </div>
       </section>
