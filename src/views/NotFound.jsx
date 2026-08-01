d9e47bbf1057d78d9b4b8d880d334d2908ac17af
Murat Akkus <akkusmurat123@gmail.com>
2026-08-01 02:15:00 +0200

    fix: replace vite scaffold title/favicon and style the 404 page

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+import "../assets/css/views/NotFound.css";
+
 function NotFound() {
   return (
-    <>
-      <section className={`page__container--grid product__list--container `}>
-        Not Found
-      </section>
-    </>
+    <section className="not-found__container page__container--flex-x-centered">
+      <h1 className="not-found__code">404</h1>
+      <p className="not-found__message">
+        We couldn&apos;t find the page you were looking for.
+      </p>
+      <Link to="/" className="not-found__link">
+        Back to the catalog
+      </Link>
+    </section>
   );
 }
 
